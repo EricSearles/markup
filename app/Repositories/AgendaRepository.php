d0509e9cd0cd3acfea780902ee85f4f240cf8189
EricSearles <ericsearles@gmail.com>
2022-07-23 12:46:42 -0300

    Criação dos arquivos para fazer a agenda

---
+++ app/Repositories/AgendaRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\Repository\RepositoryInterface;
+
+class AgendaRepository extends Repository implements RepositoryInterface
+{
+
+}
---
+++ app/Repositories/HorarioRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\Repository\RepositoryInterface;
+
+class HorarioRepository extends Repository implements RepositoryInterface
+{
+
+}
